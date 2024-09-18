6b41caf7b3598ab9d30dd29e0e3269511bb6298d
barlog <barlog7@yandex.ru>
2024-09-18 16:46:35 +0300

    add bages

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Barlog7/java-project-99/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Barlog7/java-project-99/actions)
+<a href="https://codeclimate.com/github/Barlog7/java-project-99/maintainability"><img src="https://api.codeclimate.com/v1/badges/de0dc354f37c99b3fd0a/maintainability" /></a>
+<a href="https://codeclimate.com/github/Barlog7/java-project-99/test_coverage"><img src="https://api.codeclimate.com/v1/badges/de0dc354f37c99b3fd0a/test_coverage" /></a>
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.6"
 	checkstyle
 	application
-	id("com.github.ben-manes.versions") version "0.48.0"
+	jacoco
 }
 
 group = "hexlet.code"
@@ -33,11 +33,29 @@ dependencies {
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	annotationProcessor("org.projectlombok:lombok")
+
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("org.junit.jupiter:junit-jupiter")
+	testImplementation(platform("org.junit:junit-bom:5.9.1"))
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+	testLogging {
+		//exceptionFormat = TestExceptionFormat.FULL
+		//events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+		// showStackTraces = true
+		// showCauses = true
+		showStandardStreams = true
+	}
+	finalizedBy(tasks.jacocoTestReport)
+}
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)// tests are required to run before generating the report
+	reports {
+		xml.required = true
+	}
 }
