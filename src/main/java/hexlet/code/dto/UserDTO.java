0d8568b855a3308fc8f3418cd3d7a7d3fc82e95c
barlog <barlog7@yandex.ru>
2024-11-14 15:16:27 +0300

    try to fix dto

--- src/main/java/hexlet/code/dto/UserDTO.java
+++ src/main/java/hexlet/code/dto/UserDTO.java
@@ -25,6 +25,6 @@ public class UserDTO {
     private Date createdAt;
     //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss.SSSZ")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private Date updatedAt;
+    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    //private Date updatedAt;
 }
