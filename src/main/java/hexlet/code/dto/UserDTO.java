a5dc2b1399f31bcf3be53dc335e63b8a3b4340f9
barlog <barlog7@yandex.ru>
2024-11-15 12:50:45 +0300

    fix userDTO JsonFormat

--- src/main/java/hexlet/code/dto/UserDTO.java
+++ src/main/java/hexlet/code/dto/UserDTO.java
@@ -20,8 +20,8 @@ public class UserDTO {
 
     private String email;
 
-    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createdAt;
     //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss.SSSZ")
